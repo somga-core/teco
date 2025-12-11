b903b3e40c3d692ffabd651357bd497650ac690b
K0RSY <pgeorg2010@gmail.com>
2025-12-11 18:02:25 +0200

    Added sprite drawing

--- example.cpp
+++ example.cpp
@@ -1,22 +1,34 @@
 #include "teco.hpp"
 
-void tick_tock() {
-    std::cout << "TICKIFICATION!!!" << std::endl;
-}
+void tick_tock();
+void draw_drew();
 
-void draw_drew() {
-    std::cout << "DRAWIFICATION!!!" << std::endl;
-}
+teco::Screen mein_screen = teco::Screen(192, 56, &tick_tock, &draw_drew);
+
+teco::Sprite test_sprite = teco::Sprite(
+    std::vector<teco::Animation> {
+        teco::Animation(
+            std::vector<teco::Source> {
+                teco::Source("assets/example.tcsb", "assets/example.tccl", "assets/example.tcef")
+            }
+        )
+    }
+);
 
 int main(int argc, char const *argv[])
 {
     teco::init();
-
-    teco::Screen mein_screen = teco::Screen(100, 100, &tick_tock, &draw_drew);
 
     teco::current_screen = &mein_screen;
 
     teco::mainloop();
 
     return 0;
+}
+
+void draw_drew() {
+    mein_screen.draw_sprite(0, 0, test_sprite);
+}
+
+void tick_tock() {
 }
